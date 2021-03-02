7b1256df04a249c2be671125616756f3559ce41b
Alex Shevchenko <shevchenko131sm58@gmail.com>
2021-03-02 02:35:23 +0200

    java

---
+++ n even or odd.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    int n=5;
+        if(n == 2) {
+        System.out.println("Число n - четное");
+    }
+        else {
+        System.out.println("Число n - нечетное");
+
+    }
+
+    }
